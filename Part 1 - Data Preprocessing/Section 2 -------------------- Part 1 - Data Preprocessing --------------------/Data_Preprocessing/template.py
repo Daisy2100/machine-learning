9466ab5b5fa4eeaf51a5745a592bc4a4895bdb92
Daisy2100 <flin951753@gmail.com>
2024-10-28 21:03:13 +0800

    第五節

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/template.py
@@ -13,22 +13,6 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Taking care of missing data
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
-
-# Encoding categorical data
-# Encoding the Independent Variable
-ct = ColumnTransformer(
-    transformers=[('encoder', OneHotEncoder(), [0])], 
-    remainder='passthrough'
-)
-X = ct.fit_transform(X)
-
-# Encoding the Dependent Variable
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
-
 # Splitting the dataset into the Training set and Test set
 test_size = 0.2
 train_size = 0.8
@@ -37,6 +21,6 @@ random_state = 0
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 # Feature Scaling
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.transform(X_test)
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # machine learning
 
+常規操作
+
+1. 打開 ANACONDA
+2. 打開 Spyder
+3. import 
+![alt text](./readmeImages/image.png)
+
+
 
 https://www.superdatascience.com/pages/%E4%B8%8B%E8%BD%BD%E6%95%B0%E6%8D%AE%E9%9B%86
